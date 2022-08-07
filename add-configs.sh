a0303b999d1e7c65ca2fef98fc3e61c0f93c0631
intrudir <24526564+intrudir@users.noreply.github.com>
2022-08-07 16:50:52 -0400

    init commit

---
+++ add-configs.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# CTF vim config
+vim_rc="
+\" turn on syntax highlight
+syntax on
+
+\" turn on line numbers
+set number
+
+\" make backspace work as intended in edit mode
+set backspace=indent,eol,start
+
+\" Indentation
+set shiftwidth=4
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+
+\" Auto indent what you can
+set autoindent
+
+\" Display
+set ls=2
+set noshowmode
+set showcmd
+set modeline
+set ruler
+set title
+set nu
+
+\" History
+set history=50
+
+\" Command shortcuts
+\" sort the buffer removing duplicates
+nmap <Leader>s :%!sort -u --version-sort<CR>
+
+\" Base64 decode word under cursor
+nmap <Leader>b :!echo <C-R><C-W> \| base64 -d<CR>
+
+\" Pretty print XML
+nmap <Leader>x :!xmllint --format -
+"
+echo "$vim_rc" > ~/.vimrc
+
+# CTF shell aliases
+zsh_rc="
+### my aliases
+
+# required to sudo an aliased command
+alias sudo='sudo '
+
+# tmux
+alias tml='tmux ls'
+alias tma='tmux a -t'
+alias tmn='tmux new -s'
+
+# binaries
+alias vi='vim'
+alias ngrok='/opt/ngrok'
+
+# web
+alias secretfinder='python3 /opt/SecretFinder/SecretFinder.py'
+alias linkfinder='python3 /opt/LinkFinder/linkfinder.py'
+
+# my_tools
+alias bypassfuzzer='python3 ~/my_tools/bypassfuzzer/bypassfuzzer.py'
+
+# Proxy stuff
+alias burl='curl -x 127.0.0.1:8080 -k'
+alias murl=\"curl -x 127.0.0.1:8080 -k -H $'X-Pwnfox-Color: magenta'\"
+alias proxypy='export REQUESTS_CA_BUNDLE=\"path/to/burpcert.pem\"; export HTTP_PROXY=\"http://127.0.0.1:8080\"; export HTTPS_PROXY=\"http://127.0.0.1:8080\"'
+alias proxypy_unset='unset REQUESTS_CA_BUNDLE; unset HTTP_PROXY; unset HTTPS_PROXY'
+
+# shortcuts
+alias POST='curl -d @-'
+alias apachelogs=\"tail -f -n 10 /var/log/apache2/access.log\"
+alias udc='python3 -c \"import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))\"'
+alias uec='python3 -c \"import sys, urllib.parse as ul; print(ul.quote_plus(sys.argv[1]))\"'
+
+# other
+export EDITOR=vim
+"
+echo "$zsh_rc" >> ~/.zshrc
+
+# CTF tmux.conf
+tmux_conf="
+### enable mouse:
+set -g mouse on
+
+### Scrollback buffer
+set -g history-limit 65000
+
+### get 256 colors to work, including in vim/nvim
+set-option -g default-terminal \"screen-256color\"
+set -ga terminal-overrides \",xterm-256color:RGB\"
+
+### fix tmux escape time (for vim/nvim)
+set-option -sg escape-time 10
+
+### New windows open in current path
+bind c new-window -c \"#{pane_current_path}\"
+
+### New panes open in current path
+bind '\"' split-window -c \"#{pane_current_path}\"
+bind % split-window -h -c \"#{pane_current_path}\"
+"
+echo "$tmux_conf" > ~/.tmux.conf
+
---
+++ vm-setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function check_if_success () {
+    if [ $? -eq 0 ]; then
+        echo "OK"
+    else
+        echo "Something went wrong. Stopping here so you can check the error."
+        exit
+    fi
+}
+
+# Make sure sudo is installed
+sudo -l
+check_if_success
+
+# install stuff
+sudo apt install dnsutils net-tools curl git tmux zsh wget
+check_if_success
+
+# change default shell for user
+sudo chsh -s $(which zsh) $(whoami)
+check_if_success
+
+# Install OhMyZsh!
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install necessary fonts for powerlevel10k
+wget -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+sudo mv *.ttf /usr/share/fonts
+
+sudo fc-cache -f
+check_if_success
+
+read -p "Press any key to resume after installing fonts ..."
+echo "The pk10 fonts have been installed. You may need to manually set them in your terminal if it didn't automatically do it."
+
+# install P10K
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo 'source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+echo
+echo "Your default shell has been changed. You need to log out completely and back in for it to take effect!"
+echo "When logging back in, open your terminal and configure P10K how you like it."
+echo
+echo "Now run add-configs.sh to install tmux, vim, alias configs!"
+echo
+exit
