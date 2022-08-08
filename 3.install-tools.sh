c75d1c2ef3a914a13a9d80e5846e580c14bcf5d7
intrudir <24526564+intrudir@users.noreply.github.com>
2022-08-08 10:59:14 -0400

    added favorite golang tools

--- 3.install-tools.sh
+++ 3.install-tools.sh
@@ -11,15 +11,38 @@ curl -OJ -L --progress-bar "$LATEST_GO_DOWNLOAD"
 printf "Extracting file...\n"
 tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
 
-
-
-golang_path="
+golang_path='
 
 # golang stuff
 export GOROOT="/home/$USER/go"
 export GOPATH="/home/$USER/go/packages"
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 
-"
+'
 echo "$golang_path" >> ~/.zshrc
 
+source ~/.zshrc
+
+# anew
+go install github.com/tomnomnom/anew@latest
+
+# nuclei
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+# nuclei templates
+nuclei -update-templates
+
+# httpx
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+# httprobe
+go install github.com/tomnomnom/httprobe@latest
+
+# interactsh client
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
+
+# interactsh server
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-server@latest
+
+# amass
+go install -v github.com/OWASP/Amass/v3/...@master
--- README.md
+++ README.md
@@ -8,11 +8,11 @@ No need to `git clone`. From your linux machine simply...
 
 Run the following and follow the prompts.
 ```bash
-wget https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/vm-setup.sh
+wget https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/1.vm-setup.sh
 
-chmod +x vm-setup.sh
+chmod +x 1.vm-setup.sh
 
-./vm-setup.sh
+./1.vm-setup.sh
 ```
 
 <br>
@@ -20,9 +20,21 @@ chmod +x vm-setup.sh
 Once you log out and back in, open terminal and configure p10k accoridng to the prompts. Then:
 
 ```bash
-wget https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/add-configs.sh
+wget https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/2.add-configs.sh
 
-chmod +x add-configs.sh
+chmod +x 2.add-configs.sh
 
-./add-configs.sh
+./2.add-configs.sh
+```
+
+<br>
+
+Install my favorite tools:
+
+```bash
+wget https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/3.install-tools.sh
+
+chmod +x 3.install-tools.sh
+
+./3.install-tools.sh
 ```
