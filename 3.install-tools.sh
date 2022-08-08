c0576d7187d6fee680ac2d5a1f3b677f90203b83
intrudir <24526564+intrudir@users.noreply.github.com>
2022-08-08 11:16:11 -0400

    Added OS & Arch check for golang install.

--- 3.install-tools.sh
+++ 3.install-tools.sh
@@ -1,6 +1,45 @@
+# Get user's OS and arch
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
+ARCH="$(uname -m)"
+
+case $OS in
+    "linux")
+        case $ARCH in
+        "x86_64")
+            ARCH=amd64
+            ;;
+        "aarch64")
+            ARCH=arm64
+            ;;
+        "armv6" | "armv7l")
+            ARCH=armv6l
+            ;;
+        "armv8")
+            ARCH=arm64
+            ;;
+        .*386.*)
+            ARCH=386
+            ;;
+        esac
+        PLATFORM="linux-$ARCH"
+    ;;
+    "darwin")
+          case $ARCH in
+          "x86_64")
+              ARCH=amd64
+              ;;
+          "arm64")
+              ARCH=arm64
+              ;;
+          esac
+        PLATFORM="darwin-$ARCH"
+    ;;
+esac
+
 # Install the latest golang
 LATEST_GO_VERSION=$(curl "https://go.dev/VERSION?m=text")
-LATEST_GO_DOWNLOAD="https://golang.org/dl/$LATEST_GO_VERSION.linux-amd64.tar.gz"
+GO_TAR="$LATEST_GO_VERSION.$OS-$ARCH.tar.gz"
+LATEST_GO_DOWNLOAD="https://golang.org/dl/$GO_TAR"
 
 printf "cd to home ($USER) directory \n"
 cd "/home/$USER"
@@ -9,9 +48,9 @@ printf "Downloading ${LATEST_GO_DOWNLOAD}\n\n";
 curl -OJ -L --progress-bar "$LATEST_GO_DOWNLOAD"
 
 printf "Extracting file...\n"
-tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
+tar -xf "$GO_TAR"
 
-golang_path='
+GOLANG_PATH='
 
 # golang stuff
 export GOROOT="/home/$USER/go"
@@ -19,7 +58,7 @@ export GOPATH="/home/$USER/go/packages"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 
 '
-echo "$golang_path" >> ~/.zshrc
+echo "$GOLANG_PATH" >> ~/.zshrc
 
 source ~/.zshrc
 
