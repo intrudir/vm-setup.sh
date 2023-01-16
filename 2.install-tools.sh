e58f4eb6fe34805b6500280dad7422b7cfb5b5ba
intrudir <24526564+intrudir@users.noreply.github.com>
2023-01-16 11:46:09 -0500

    added .bash_aliases

--- 2.install-tools.sh
+++ 2.install-tools.sh
@@ -14,132 +14,13 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 check_if_success
 
 # CTF vim config
-vim_rc="
-\" Specify a directory for plugins
-\" - For Neovim: stdpath('data') . '/plugged'
-\" - Avoid using standard Vim directory names like 'plugin'
-call plug#begin()
-
-Plug 'junegunn/vim-easy-align'
-Plug 'preservim/nerdtree'
-Plug 'simnalamburt/vim-mundo'
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-Plug 'AndrewRadev/splitjoin.vim'
-Plug 'preservim/nerdcommenter'
-Plug 'jalvesaq/vimcmdline'
-
-call plug#end()
-
-\" airline stuff
-set laststatus=2
-let g:airline_theme='simple'
-
-\" NERDTree stuff
-\" NERDTree remaps
-nnoremap <leader>n :NERDTreeFocus<CR>
-nnoremap <C-n> :NERDTree<CR>
-nnoremap <C-t> :NERDTreeToggle<CR>
-nnoremap <C-f> :NERDTreeFind<CR>
-
-\" Mundo stuff
-\" Enable persistent undo so that undo history persists across vim sessions
-set undofile
-set undodir=~/.vim/undo
-
-\" Mundo remaps
-nnoremap <F5> :MundoToggle<CR>
-
-\" easy-align
-\" Start interactive EasyAlign in visual mode (e.g. vipga)
-xmap ga <Plug>(EasyAlign)
-
-\" Start interactive EasyAlign for a motion/text object (e.g. gaip)
-nmap ga <Plug>(EasyAlign)
-
-\" turn on syntax highlight
-syntax on
-
-\" turn on line numbers
-set number
-
-\" make backspace work as intended in edit mode
-set backspace=indent,eol,start
-
-\" Indentation
-set shiftwidth=4
-set tabstop=4
-set softtabstop=4
-set shiftwidth=4
-set expandtab
-
-\" Auto indent what you can
-set autoindent
-
-\" Display
-set ls=2
-set noshowmode
-set showcmd
-set modeline
-set ruler
-set title
-set nu
-
-\" History
-set history=50
-
-\" Command shortcuts
-\" sort the buffer removing duplicates
-nmap <Leader>s :%!sort -u --version-sort<CR>
-
-\" Base64 decode word under cursor
-nmap <Leader>b :!echo <C-R><C-W> \| base64 -d<CR>
-
-\" Pretty print XML
-nmap <Leader>x :!xmllint --format -
-"
+vim_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/vimrc)
 echo "$vim_rc" > ~/.vimrc
 
 # CTF shell aliases
-zsh_rc="
-### my aliases
-
-# required to sudo an aliased command
-alias sudo='sudo '
-
-# tmux
-alias tml='tmux ls'
-alias tma='tmux a -t'
-alias tmn='tmux new -s'
-
-# binaries
-alias vi='vim'
-alias ngrok='/opt/tools/ngrok'
-
-# web
-alias secretfinder='python3 /opt/tools/SecretFinder/SecretFinder.py'
-alias linkfinder='python3 /opt/tools/LinkFinder/linkfinder.py'
-
-# my_tools
-alias bypassfuzzer='python3 ~/my_tools/bypassfuzzer/bypassfuzzer.py'
-
-# Proxy stuff
-alias burl='curl -x 127.0.0.1:8080 -k'
-alias murl=\"curl -x 127.0.0.1:8080 -k -H $'X-Pwnfox-Color: magenta'\"
-alias proxypy='export REQUESTS_CA_BUNDLE=\"path/to/burpcert.pem\"; export HTTP_PROXY=\"http://127.0.0.1:8080\"; export HTTPS_PROXY=\"http://127.0.0.1:8080\"'
-alias proxypy_unset='unset REQUESTS_CA_BUNDLE; unset HTTP_PROXY; unset HTTPS_PROXY'
-
-# shortcuts
-alias POST='curl -d @-'
-alias apachelogs=\"tail -f -n 10 /var/log/apache2/access.log\"
-alias udc='python3 -c \"import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))\"'
-alias uec='python3 -c \"import sys, urllib.parse as ul; print(ul.quote_plus(sys.argv[1]))\"'
-
-# other
-export EDITOR=vim
-"
-
+zsh_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-aliases)
 echo "$zsh_rc" >> ~/.zshrc
+echo "$zsh_rc" >> ~/.bash_aliases
 
 # Install tmux themes
 sudo mkdir /opt/tmux && cd /opt/tmux
@@ -148,30 +29,9 @@ sudo git clone https://github.com/wfxr/tmux-power.git
 cd ~
 
 # CTF tmux.conf
-tmux_conf="
-### enable mouse:
-set -g mouse on
-
-### Scrollback buffer
-set -g history-limit 65000
-
-### get 256 colors to work, including in vim/nvim
-set-option -g default-terminal \"screen-256color\"
-set -ga terminal-overrides \",xterm-256color:RGB\"
-
-### fix tmux escape time (for vim/nvim)
-set-option -sg escape-time 10
-
-### New windows open in current path
-bind c new-window -c \"#{pane_current_path}\"
-
-### New panes open in current path
-bind '\"' split-window -c \"#{pane_current_path}\"
-bind % split-window -h -c \"#{pane_current_path}\"
-
-run-shell \"/opt/tmux/tmux-power/tmux-power.tmux\"
-"
+tmux_conf=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-tmux.conf)
 echo "$tmux_conf" > ~/.tmux.conf
+
 echo
 echo "Now refresh or restart your terminal session!"
 
