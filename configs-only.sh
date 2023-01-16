542c19f090e01281a723343f23776d5dfc166195
intrudir <24526564+intrudir@users.noreply.github.com>
2023-01-16 11:32:40 -0500

    Refactoring

---
+++ configs-only.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+function check_if_success () {
+    if [ $? -eq 0 ]; then
+        echo "OK"
+    else
+        echo "Something went wrong. Stopping here so you can check the error."
+        exit
+    fi
+}
+# Install VIM plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+check_if_success
+# CTF vim config
+vim_rc=""
+echo "$vim_rc" > ~/.vimrc
+
+# CTF shell aliases
+zsh_rc=""
+echo "$zsh_rc" >> ~/.zshrc
+
+# Install tmux themes
+sudo mkdir /opt/tmux && cd /opt/tmux
+check_if_success
+sudo git clone https://github.com/wfxr/tmux-power.git
+cd ~
+
+# CTF tmux.conf
+tmux_conf=""
+echo "$tmux_conf" > ~/.tmux.conf
+
+echo
+echo "Now refresh or restart your terminal session!"
