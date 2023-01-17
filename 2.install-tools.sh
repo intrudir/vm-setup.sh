8cd4e43c694a2fbd39a48f1cded42a021467db70
intrudir <24526564+intrudir@users.noreply.github.com>
2023-01-17 13:52:53 -0500

    Moved golang install to separate script

--- 2.install-tools.sh
+++ 2.install-tools.sh
@@ -32,76 +32,12 @@ cd ~
 tmux_conf=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-tmux.conf)
 echo "$tmux_conf" > ~/.tmux.conf
 
-echo
-echo "Now refresh or restart your terminal session!"
-
-source ~/.zshrc
-
-# Get user's OS and arch
-OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
-ARCH="$(uname -m)"
-
-case $OS in
-    "linux")
-        case $ARCH in
-        "x86_64")
-            ARCH=amd64
-            ;;
-        "aarch64")
-            ARCH=arm64
-            ;;
-        "armv6" | "armv7l")
-            ARCH=armv6l
-            ;;
-        "armv8")
-            ARCH=arm64
-            ;;
-        .*386.*)
-            ARCH=386
-            ;;
-        esac
-        PLATFORM="linux-$ARCH"
-    ;;
-    "darwin")
-          case $ARCH in
-          "x86_64")
-              ARCH=amd64
-              ;;
-          "arm64")
-              ARCH=arm64
-              ;;
-          esac
-        PLATFORM="darwin-$ARCH"
-    ;;
-esac
-
 # Install the latest golang
-LATEST_GO_VERSION="$(curl "https://go.dev/VERSION?m=text")"
-GO_TAR="$LATEST_GO_VERSION.$OS-$ARCH.tar.gz"
-LATEST_GO_DOWNLOAD="https://golang.org/dl/$GO_TAR"
-
-printf "cd to home ($USER) directory \n"
-cd "/home/$USER"
-
-printf "Downloading ${LATEST_GO_DOWNLOAD}\n\n";
-curl -OJ -L --progress-bar "$LATEST_GO_DOWNLOAD"
+wget https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/install-golang.sh
+chmod +x ./install-golang.sh
+./install-golang.sh
 check_if_success
-
-printf "Extracting file...\n"
-tar -xf "$GO_TAR"
-check_if_success
-
-GOLANG_PATH='
-
-# golang stuff
-export GOROOT="$HOME/go"
-export GOPATH="$HOME/go/packages"
-export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
-
-'
-
-echo "$GOLANG_PATH" >> ~/.zshrc
-
+rm ./install-golang.sh
 source ~/.zshrc
 
 # anew
@@ -159,6 +95,7 @@ deactivate
 
 # install zsh autosuggestions and syntax highlighting
 sudo apt install zsh-autosuggestions zsh-syntax-highlighting
+check_if_success
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
