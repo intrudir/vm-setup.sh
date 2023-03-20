ebd2a314f4fb50352fdd064de88c3bc12ef309b7
intrudir <24526564+intrudir@users.noreply.github.com>
2023-03-20 17:16:40 -0400

    updated config install

--- install-configs.sh
+++ install-configs.sh
@@ -27,45 +27,45 @@ if [[ ! $type == 'full' ]] && [[ ! $type == 'ctf' ]]; then
     exit 1
 fi
 
-echo "install-configs VM type: $type";
+echo "VM type: $type";
 
 if [[ $type == 'full' ]]; then
     echo "Full vim config"
-    # vim_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/full-vimrc)
-    # echo "$vim_rc" > ~/.vimrc
+    vim_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/full-vimrc)
+    echo "$vim_rc" > ~/.vimrc
 
     echo "Full shell aliases"
-    # zsh_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/full-aliases)
-    # echo "$zsh_rc" >> ~/.zshrc
-    # echo "$zsh_rc" >> ~/.bash_aliases
+    zsh_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/full-aliases)
+    echo "$zsh_rc" >> ~/.zshrc
+    echo "$zsh_rc" >> ~/.bash_aliases
 
     echo "Full tmux.conf"
-    # tmux_conf=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/full-tmux.conf)
-    # echo "$tmux_conf" > ~/.tmux.conf
+    tmux_conf=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/full-tmux.conf)
+    echo "$tmux_conf" > ~/.tmux.conf
 
     echo "Install VIM plug"
-    # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    # check_if_success
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    check_if_success
 
     echo "Install tmux themes"
-    # sudo mkdir /opt/tmux && cd /opt/tmux
-    # check_if_success
-    # sudo git clone https://github.com/wfxr/tmux-power.git
-    # cd ~
+    sudo mkdir /opt/tmux && cd /opt/tmux
+    check_if_success
+    sudo git clone https://github.com/wfxr/tmux-power.git
+    cd ~
 fi
 
 
 if [[ $type == 'ctf' ]]; then
     echo "CTF vim config"
-    # vim_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-vimrc)
-    # echo "$vim_rc" > ~/.vimrc
+    vim_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-vimrc)
+    echo "$vim_rc" > ~/.vimrc
 
     echo "CTF shell aliases"
-    # zsh_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-aliases)
-    # echo "$zsh_rc" >> ~/.zshrc
-    # echo "$zsh_rc" >> ~/.bash_aliases
+    zsh_rc=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-aliases)
+    echo "$zsh_rc" >> ~/.zshrc
+    echo "$zsh_rc" >> ~/.bash_aliases
 
     echo "CTF tmux.conf"
-    # tmux_conf=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-tmux.conf)
-    # echo "$tmux_conf" > ~/.tmux.conf
+    tmux_conf=$(curl https://raw.githubusercontent.com/intrudir/vm-setup.sh/main/dotfiles/ctf-tmux.conf)
+    echo "$tmux_conf" > ~/.tmux.conf
 fi
